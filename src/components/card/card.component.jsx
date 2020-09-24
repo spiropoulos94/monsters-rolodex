910fc0282539819790b50f28081e12d569bbcaa4
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-24 16:23:29 +0300

    card list component added

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,1 +1,11 @@
-rcc
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1 key={monster.id}>{monster.name}</h1>
+        </div>
+    );
+};
+
+export default MyComponent;
