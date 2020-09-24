f8bff2fb125370a531875be11735a85469a2b0df
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-09-24 17:39:03 +0300

    card component added along with first styles

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./card.styles.css"
 
-const MyComponent = () => {
+const Card = (props) => {
     return (
-        <div>
-            <h1 key={monster.id}>{monster.name}</h1>
+        <div className={"card-container"}>
+            <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
+            <h2 key={props.monster.id}>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
         </div>
     );
 };
 
-export default MyComponent;
+export default Card;
--- src/components/cardlist/cardlist.component.jsx
+++ src/components/cardlist/cardlist.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./cardlist.styles.css"
+import Card from "../card/card.component";
+
 
 const CardList = (props) => {
 
     return (
         <div className={'card-list'}>
-            {props.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+            {props.monsters.map(monster =>
+                <Card monster={monster}/>
+            )}
         </div>
     );
 };
