2492a48037422521c5dfaa2465415caa1a327ded
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-09-27 23:50:42 +0300

    last files added. styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,29 @@ class App extends Component {
             monsters: [],
             searchField: ""
         }
+
+        // Bind creates a new function that will force the this inside the function to be the parameter passed to bind().
+
+        //JAVASCRIPT BY DEFAULT DOESNT SET THE SCOPE OF THIS ON FUNCTIONS. YOU HAVE TO DECLARE IT EXPLICITLY.
+        //olo ayto to kanoume ston contructor giati trexei prwtos kai theloume na vevaiwthoume oti this anaferetai
+        // swsta se kathe method kai oxi ws undefined. gia na mhn ginei lathos
+        //in this case we want to bind this on the app component
+
+        //Otan exoume arrow functions to this mesa tous AYTOMATA ginetai bind sto meros pou kanoume declare thn
+        // arrow function, diladi sto context tou component px.
+
+        // this.handleChange = this.handleChange.bind(this)
+
     }
 
     componentDidMount() {
         fetch(url)
             .then(res => res.json())
             .then(data => this.setState({monsters: data}))
+    }
+
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value})
     }
 
 
@@ -30,11 +47,12 @@ class App extends Component {
 
         return (
             <div className="App">
+                <h1> Monsters Rolodex</h1>
                 <SearchBox
                     placeholder="Search for Monsters"
-                    handleChange={e => this.setState({searchField: e.target.value})}/>
+                    handleChange={this.handleChange}/>
 
-                    <CardList monsters={filteredMonsters}/>
+                <CardList monsters={filteredMonsters}/>
             </div>
         )
     }
