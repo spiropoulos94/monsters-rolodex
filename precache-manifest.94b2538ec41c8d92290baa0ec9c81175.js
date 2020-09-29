092a82f66e91ed6859c2457e85642c3973f06264
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-09-29 20:00:01 +0300

    Updates

---
+++ precache-manifest.94b2538ec41c8d92290baa0ec9c81175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe9f9acf33a42f3080aa1cfbd68bb2e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "160e0e8876c5e975a032",
+    "url": "/monsters-rolodex/static/css/main.4764679a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160e0e8876c5e975a032",
+    "url": "/monsters-rolodex/static/js/main.b047ea92.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
